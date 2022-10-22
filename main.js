76a82dfccef07d88ce7f6d63bc016c5a9c326945
Harold Arriola Nucamendi <hnucamendi@Harolds-Mini.attlocal.net>
2022-10-22 00:46:56 -0400

    working on vh

--- main.js
+++ main.js
@@ -1,46 +1,66 @@
 "use strict";
-// EASY: Write a function that takes in an array of numbers and outputs the average of all the numbers.
-// Example 1:
-// Input: [1 , 4 , 7]  Output: 4
-// Input: [10, 5]  Output: 7.5
-// Input: [1.5, 3, 2.5, 1]  Output: 2
 
 /**
+ * EASY:
+ * Write a function that takes in an array of numbers and outputs the average of all the numbers.
  *
- * @param {Array} arr
- * @returns
+ * @param {Array} nums - Array of numbers
+ * @param {number} sum - Calculated sum
+ * @returns {number} Calculated average
  */
 
-const fun = (arr) => {
+const calculateAverage = (nums) => {
   let sum = 0;
-  let average = 0;
-  for (let n of arr) {
+  for (let n of nums) {
     sum += n;
   }
-  average = sum / arr.length;
-  return average;
+  return sum / nums.length;
 };
 
-console.log(fun([1, 4, 7])); // Output: 4
+// console.log(calculateAverage([1, 4, 7])); // Output: 4
 
-console.log(fun([10, 5])); // Output: 7.5
+// console.log(calculateAverage([10, 5])); // Output: 7.5
 
-console.log(fun([1.5, 3, 2.5, 1])); // Output: 2
+// console.log(calculateAverage([1.5, 3, 2.5, 1])); // Output: 2
 
-// MEDIUM: Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand. (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
-// You are given a target value to search. If found in the array return its index, otherwise return -1.
-// You may assume no duplicate exists in the array.
-// Hint:  Use a function. Use the built in method .indexOf( ) and/or  for loops. Review your lesson on Arrays to aid in working through this problem.
-// Example 1:
-// Input: nums = [4,5,6,7,0,1,2], target = 0
-// Output: 4
-// Example 2:
-// Input: nums = [4,5,6,7,0,1,2], target = 3
-// Output: -1.
+/**
+ * MEDIUM:
+ * Suppose an array is numbers with a random order of numbers unknown to you beforehand.
+ * You are given a target value to search. If found in the array return its index, otherwise return -1.
+ * You may assume no duplicate exists in the array.
+ *
+ * @param {Array} nums - Array of numbers
+ * @param {number} target - Value to search for
+ */
 
-// HARD: Create two buttons, one that when clicked turns the background of the body red. The other, when clicked, turns the body's background color white. Each of the background colors should be defined as class styles and when the buttons are pushed the body's classList is updated to include the correct class for the background.
-// Create a single function that takes an input className and updates the body's classList to ONLY include that className
-// *Note: Check Canvas for the starter code needed for this exercise. The starter code will be located in a folder named hard_js.
+const findTarget = (nums, target) => nums.indexOf(target);
+
+// console.log(findTarget([4, 5, 6, 7, 0, 1, 2], 0)); // Output: 4
+// console.log(findTarget([4, 5, 6, 7, 0, 1, 2], 3)); // Output: -1
+
+/**
+ * HARD:
+ * Create two buttons, one that turns the background color red when clicked.
+ * The other, when clicked, turns the body's background color blue.
+ *
+ */
+
+// const buttonOne = document.querySelector(".button-one");
+// const buttonTwo = document.querySelector(".button-two");
+
+// buttonOne.addEventListener("mousedown", () => {
+//   buttonOne.style.backgroundColor = "red";
+//   buttonOne.addEventListener("mouseup", () => {
+//     buttonOne.style.backgroundColor = "";
+//   });
+// });
+
+// buttonTwo.addEventListener("mousedown", () => {
+//   buttonTwo.style.backgroundColor = "blue";
+//   buttonTwo.addEventListener("mouseup", () => {
+//     buttonTwo.style.backgroundColor = "";
+//   });
+// });
 
 // VERY HARD: You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
 // Hint:  Sudo code this problem. Focus on breaking the problem down into steps  Use functions, arrays and logical operators.  Do not have anyone give you the answer or solve this problem for you.
@@ -53,3 +73,33 @@ console.log(fun([1.5, 3, 2.5, 1])); // Output: 2
 // Output: -1
 
 // Note: You may assume that you have an infinite number of each kind of coin.
+
+/**
+ * VERY HARD:
+ * You are given coins of different denominations and a total amount of money amount.
+ * Write a function to compute the fewest number of coins that you need to make up that amount.
+ * If that amount of money cannot be made up by any combination of the coins, return -1.
+ *
+ */
+
+const coinCounter = (coins, target) => {
+  coins.sort((a, b) => a - b);
+  let minCoins = 0;
+  while (target > 0 ) {
+    for (let i = coins.length - 1; i > -1; i--) {
+      if (coins[i] <= target) {
+        target -= coins[i];
+        minCoins += 1;
+        break;
+      }
+      if (i === 0 && coins[i] > target) {
+        return -1;
+      }
+    }
+  }
+  return minCoins;
+};
+
+// console.log(coinCounter([1, 2, 5], 11));
+// console.log(coinCounter([2], 3));
+// console.log(coinCounter([0], 3)); brokens
